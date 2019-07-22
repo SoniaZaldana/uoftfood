0b3f6fd49f07b3cc44ecdbf21764e4468acdfe57
Sonia Zaldana Calles <sonia.calles@rbc.com>
2019-07-22 11:48:48 -0400

    Fixed splash activity

--- app/src/main/java/com/uoft/food/SplashActivity.java
+++ app/src/main/java/com/uoft/food/SplashActivity.java
@@ -10,8 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
