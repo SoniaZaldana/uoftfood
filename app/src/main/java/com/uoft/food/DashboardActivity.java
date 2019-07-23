2f1ece26ba0c24f1ee34421edc7a709a86041709
Sonia Zaldana Calles <sonia.calles@rbc.com>
2019-07-23 10:51:09 -0400

    Refactored name of layout

--- app/src/main/java/com/uoft/food/DashboardActivity.java
+++ app/src/main/java/com/uoft/food/DashboardActivity.java
@@ -10,7 +10,7 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
     }
 
     @Override
